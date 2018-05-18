b3b319b5f3a02b89c96c9bd91a56cbc5e3b8682c
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-05-18 14:45:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,7 +36,15 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
+  This is a class method that uses keyword arguments. The keyword arguments are name: and grade:.
+  Use the values of these keyword arguments to: 1) instantiate a new Student object with
+  Student.new(name, grade) and 2) save that new student object via student.save. The
+  #create method should return the student object that it creates.
 
- def create
+ def self.create(name:, grade:)
+   student = self.new(name, grade)
+   student.save
+   student
+
  end
 end
