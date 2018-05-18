6b3c2c8e20a17c0718355193a606791ea75e3fad
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-05-18 14:39:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,8 +35,8 @@ class Student
     puts sql
     DB[:conn].execute(sql, self.name, self.grade)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-
-
   end
 
+ def create
+ end
 end
